26302427cce42ce709ca37139df25f06593340e8
LAPTOP-13GSOUL7\妹尾 <kd1257172@st.kobedenshi.ac.jp>
2017-07-19 12:06:46 +0900

    7.19 おまけ

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ function answer(){
   var output = document.getElementById('output');
 
     // 文字列として結合
-    // var result = input1.value + input2.value;
+     var result = input1.value + input2.value;
 
     // 整数として足し算
-    var result = parseInt(input1.value) + parseInt(input2.value);
+    //var result = parseInt(input1.value) + parseInt(input2.value);
 
    output.textContent = result
 }
